afd2cdef3f87e14650c5def0df0b4eacf79813bf
Yichen Huang <yichen0831@gmail.com>
2015-11-02 20:05:23 +0800

    Add sorting before rendering with RenderSystem

--- core/src/com/ychstudio/systems/RenderSystem.java
+++ core/src/com/ychstudio/systems/RenderSystem.java
@@ -2,14 +2,18 @@ package com.ychstudio.systems;
 
 import com.artemis.Aspect;
 import com.artemis.ComponentMapper;
-import com.artemis.systems.IteratingSystem;
+import com.artemis.Entity;
+import com.artemis.EntitySystem;
+import com.artemis.utils.Bag;
+import com.artemis.utils.Sort;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.ychstudio.components.Renderer;
 import com.ychstudio.components.Transform;
+import java.util.Comparator;
 
-public class RenderSystem extends IteratingSystem {
+public class RenderSystem extends EntitySystem {
 
-    private SpriteBatch batch;
+    private final SpriteBatch batch;
     
     protected ComponentMapper<Transform> mTransform;
     protected ComponentMapper<Renderer> mRenderer;
@@ -30,16 +34,34 @@ public class RenderSystem extends IteratingSystem {
         batch.end();
     }
 
-    @Override
-    protected void process(int i) {
-        Transform transform = mTransform.get(i);
-        Renderer renderer = mRenderer.get(i);
+    protected void process(Entity e) {
+        Transform transform = mTransform.get(e);
+        Renderer renderer = mRenderer.get(e);
         
         renderer.setPosition(transform.posX, transform.posY);
         renderer.setRotation(transform.rotation);
         renderer.setScale(transform.sclX, transform.sclY);
         
         renderer.draw(batch);
+    }
+
+    @Override
+    protected void processSystem() {
+        Bag<Entity> entities = getEntities();
+        
+        Sort sort = Sort.instance();
+        sort.sort(entities, new Comparator<Entity>() {
+            @Override
+            public int compare(Entity o1, Entity o2) {
+                Transform t1 = mTransform.get(o1);
+                Transform t2 = mTransform.get(o2);
+                return (int) -(t1.posY - t2.posY);
+            }
+        });
+        
+        for (Entity e : entities) {
+            process(e);
+        }
     }
     
 }
